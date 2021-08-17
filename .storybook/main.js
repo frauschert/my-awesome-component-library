652721ec2ac95b6b729d6bad14a8ef5bfc20939d
Fabian Rauschert <fabian.doell@freenet.de>
2021-08-17 22:21:50 +0200

    added button in new folder structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+    "../src/**/*.stories.tsx"
+ ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
