878b113e2b1b6f0aa053b07da54f089b54f0d822
Fabian Rauschert <fabian.doell@freenet.de>
2021-10-08 22:33:22 +0200

    added useLocalStorage hook

--- src/utility/hooks/index.ts
+++ src/utility/hooks/index.ts
@@ -1,1 +1,11 @@
-export { default } from './useDebounce'
+import * as useDebounce from './useDebounce'
+import * as useContextMenu from './useContextMenu'
+import * as usePrevious from './usePrevious'
+import * as useLocalStorage from './useLocalStorage'
+
+export default {
+    useDebounce,
+    useContextMenu,
+    usePrevious,
+    useLocalStorage,
+}
