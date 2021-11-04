0d5649799a328f8b0fcb4cc19c311029dab56984
Fabian Rauschert <fabian.doell@freenet.de>
2021-11-04 22:57:09 +0100

    updated storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,5 +5,9 @@ module.exports = {
     },
 
     stories: ['../src/**/*.stories.tsx'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-controls',
+    ],
 }
