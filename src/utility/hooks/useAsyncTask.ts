262769f8a0c1b66a08c86fbdbe3861fa21e2ec7c
Fabian Rauschert <fabian.doell@freenet.de>
2022-02-06 19:50:50 +0100

    adjusted useAsyncTask hook

--- src/utility/hooks/useAsyncTask.ts
+++ src/utility/hooks/useAsyncTask.ts
@@ -8,20 +8,24 @@ function useAsyncTask<T extends any[], R = any>(
     const [status, setStatus] = useState<TStatus>('IDLE')
     const [message, setMessage] = useState('')
 
-    const run = useCallback(async (...arg: T) => {
-        setStatus('PROCESSING')
-        try {
-            const resp = await task(...arg)
-            setStatus('SUCCESS')
-            return resp
-        } catch (error) {
-            const message =
-                error?.response?.data?.error?.message || error.message
-            setMessage(message)
-            setStatus('ERROR')
-            throw error
-        }
-    }, [])
+    const run = useCallback(
+        async (...arg: T) => {
+            setStatus('PROCESSING')
+            try {
+                const resp = await task(...arg)
+                setStatus('SUCCESS')
+                return resp
+            } catch (error) {
+                if (error instanceof Error) {
+                    const message = error.message
+                    setMessage(message)
+                }
+                setStatus('ERROR')
+                throw error
+            }
+        },
+        [task]
+    )
 
     const reset = useCallback(() => {
         setMessage('')
