d74dd04d7999dfc22bba600159de0e08d8fc695c
frauschert <fabian.rauschert@freenet.de>
2022-08-04 20:49:07 +0200

    added test for generateUniqueID

---
+++ src/utility/uniqueId.test.ts
@@ -0,0 +1,13 @@
+import generateUniqueID from './uniqueId'
+
+describe('generateUniqueID', () => {
+    test('should be equal', () => {
+        const array: string[] = []
+        for (let i = 0; i < 1000; i++) {
+            array.push(generateUniqueID())
+        }
+        const set = new Set(array)
+
+        expect(array.length).toBe(set.size)
+    })
+})
