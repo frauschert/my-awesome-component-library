c9cbeb6ae93515d71735058542f9a6e52c576ef6
frauschert <fabian.rauschert@freenet.de>
2022-10-18 22:17:05 +0200

    added more sort tests

--- src/utility/__tests__/sort.test.ts
+++ src/utility/__tests__/sort.test.ts
@@ -13,3 +13,29 @@ test('should sort by age in ascending order', () => {
         { name: 'John', age: 18 },
     ])
 })
+
+test('should sort by name in descending order', () => {
+    const result = sort(
+        [
+            { name: 'John', age: 18 },
+            { name: 'Peter', age: 16 },
+        ],
+        'name',
+        'descending'
+    )
+    expect(result).toStrictEqual([
+        { name: 'Peter', age: 16 },
+        { name: 'John', age: 18 },
+    ])
+})
+
+test('should handle a single value array', () => {
+    const result = sort([{ name: 'John', age: 18 }], 'age')
+    expect(result).toStrictEqual([{ name: 'John', age: 18 }])
+})
+
+test('should handle an empty array', () => {
+    const value: { name: string; age: number }[] = []
+    const result = sort(value, 'age')
+    expect(result).toStrictEqual([])
+})
