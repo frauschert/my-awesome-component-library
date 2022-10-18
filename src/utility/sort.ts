1bbffab10df7b37d45e193ecdbc9ab00437c3df0
frauschert <fabian.rauschert@freenet.de>
2022-10-18 22:19:26 +0200

    return a new array for sort

--- src/utility/sort.ts
+++ src/utility/sort.ts
@@ -5,13 +5,15 @@ export default function sort<T, K extends keyof T>(
     sortKey: K,
     direction: SortDirection = 'ascending'
 ): T[] {
-    return data.sort((a, b) => {
-        if (a[sortKey] < b[sortKey]) {
-            return direction === 'ascending' ? -1 : 1
-        }
-        if (a[sortKey] > b[sortKey]) {
-            return direction === 'ascending' ? 1 : -1
-        }
-        return 0
-    })
+    return [
+        ...data.sort((a, b) => {
+            if (a[sortKey] < b[sortKey]) {
+                return direction === 'ascending' ? -1 : 1
+            }
+            if (a[sortKey] > b[sortKey]) {
+                return direction === 'ascending' ? 1 : -1
+            }
+            return 0
+        }),
+    ]
 }
