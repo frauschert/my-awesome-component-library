1289dd512a204fa82a121d5d1c12d8e0fe608ba1
frauschert <fabian.rauschert@freenet.de>
2022-10-24 21:59:30 +0200

    small refactoring of scan function

--- src/utility/scan.ts
+++ src/utility/scan.ts
@@ -1,19 +1,13 @@
-const scan = <T>(
+export default function scan<T>(
     reducer: (prev: T, curr: T) => T,
     initialValue: T,
     array: T[]
-) => {
+) {
     const reducedValues: T[] = []
-
     array.reduce((acc, current) => {
         const newAcc = reducer(acc, current)
-
         reducedValues.push(newAcc)
-
         return newAcc
     }, initialValue)
-
     return reducedValues
 }
-
-export default scan
