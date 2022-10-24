cc805b7a70ec69b43b9481f093bee230c790674f
frauschert <fabian.rauschert@freenet.de>
2022-10-24 22:02:41 +0200

    small refactoring of intersectionBy function

--- src/utility/intersectionBy.ts
+++ src/utility/intersectionBy.ts
@@ -1,15 +1,9 @@
 import distinctBy from './distinctBy'
 
-const intersectionBy = <
+export default function intersectionBy<
     T extends Record<K, PropertyKey>,
     K extends Extract<keyof T, PropertyKey>
->(
-    listA: T[],
-    listB: T[],
-    key: K
-) => {
+>(listA: T[], listB: T[], key: K) {
     const b = new Set(listB.map((val) => val[key]))
     return distinctBy(listA, key).filter((value) => b.has(value[key]))
 }
-
-export default intersectionBy
