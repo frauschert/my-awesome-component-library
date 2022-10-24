d3b92772f8f1c45c3f90e2b6d2d11597b625e80c
frauschert <fabian.rauschert@freenet.de>
2022-10-24 21:49:39 +0200

    added test for useLocalStorage

--- src/utility/hooks/__tests__/useLocalStorage.test.ts
+++ src/utility/hooks/__tests__/useLocalStorage.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react'
+import { act, renderHook } from '@testing-library/react'
 import { useLocalStorage } from '../useStorage'
 
 const TEST_KEY = 'key'
@@ -7,4 +7,15 @@ const TEST_VALUE = { test: 'test' }
 test('should set localStorage with default value', () => {
     const { result } = renderHook(() => useLocalStorage(TEST_KEY, TEST_VALUE))
     expect(result.current[0]).toEqual(TEST_VALUE)
+})
+
+test('should remove value from localStorage', () => {
+    const { result } = renderHook(() => useLocalStorage(TEST_KEY, TEST_VALUE))
+    expect(result.current[0]).toEqual(TEST_VALUE)
+
+    act(() => {
+        result.current[2]()
+    })
+
+    expect(result.current[0]).toBeUndefined()
 })
