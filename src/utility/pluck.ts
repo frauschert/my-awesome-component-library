fcf6ef1c3e9f5b45f1b07d5eae4435ab9d9bd532
frauschert <fabian.rauschert@freenet.de>
2022-10-24 22:01:46 +0200

    small refactoring of pluck function

--- src/utility/pluck.ts
+++ src/utility/pluck.ts
@@ -1,9 +1,7 @@
 /** Plucks the given key off of each item in the array. Returns a new array of these values. */
-const pluck = <T, K extends keyof T>(key: K, array: T[]) =>
-    array.reduce<T[K][]>((values, current) => {
+export default function pluck<T, K extends keyof T>(key: K, array: T[]) {
+    return array.reduce<T[K][]>((values, current) => {
         values.push(current[key])
-
         return values
     }, [])
-
-export default pluck
+}
