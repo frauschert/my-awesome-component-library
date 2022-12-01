47528636bdaecf288532418f7842b8c7e106209a
frauschert <fabian.rauschert@freenet.de>
2022-12-01 21:16:35 +0100

    refactored Accordion.tsx

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './accordion.scss'
 
-const Accordion = () => {
+type AccordionProps = { items: AccordionItemProps[] }
+type AccordionItemProps = { title: React.ReactNode; content: React.ReactNode }
+
+const Accordion = ({ items }: AccordionProps) => {
     return (
-        <>
-            <details>
-                <summary>Headline 1</summary>
-                <div>
-                    <p>Test</p>
-                </div>
-            </details>
-            <details>
-                <summary>Headline 2</summary>
-                <div>
-                    <p>Test</p>
-                </div>
-            </details>
-        </>
+        <div className="accordion">
+            {items.map((itemProps, index) => (
+                <AccordionItem key={index} {...itemProps} />
+            ))}
+        </div>
+    )
+}
+
+function AccordionItem({ title, content }: AccordionItemProps) {
+    const [isActive, setIsActive] = useState(false)
+
+    return (
+        <div className="accordion-item">
+            <div
+                className="accordion-title"
+                onClick={() => setIsActive(!isActive)}
+            >
+                <div>{title}</div>
+                <div>{isActive ? '-' : '+'}</div>
+            </div>
+            {isActive && <div className="accordion-content">{content}</div>}
+        </div>
     )
 }
 
