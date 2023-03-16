4250ac4a637d454f66f6df86231bb64ffe15befb
frauschert <fabian.rauschert@freenet.de>
2023-03-16 23:02:29 +0100

    updated useAsync and write better tests

--- src/utility/hooks/__tests__/useAsync.test.ts
+++ src/utility/hooks/__tests__/useAsync.test.ts
@@ -1,30 +1,56 @@
 import { renderHook, act, waitFor } from '@testing-library/react'
 import useAsync from '../useAsync'
 
-async function testFunction() {
-    return new Promise<string>((resolve) =>
-        setTimeout(() => resolve('success'), 500)
-    )
-}
-
-test('should use async', async () => {
-    const { result } = renderHook(() => useAsync(testFunction, false))
-
-    expect(result.current.loading).toBe(false)
-    expect(result.current.value).toBe(null)
-    expect(result.current.error).toBe(null)
-
-    act(() => {
-        result.current.execute()
+const mockSuccess = () =>
+    Promise.resolve({
+        data: 'Mock data',
     })
 
-    expect(result.current.loading).toBe(true)
+const mockError = () => Promise.reject(new Error('An error occurred'))
 
-    await waitFor(
-        () => {
-            expect(result.current.loading).toBe(false)
-            expect(result.current.value).toBe('success')
-        },
-        { timeout: 1000 }
-    )
+describe('useAsync', () => {
+    it('should start with loading state', () => {
+        const { result } = renderHook(() => useAsync(mockSuccess))
+        expect(result.current.isLoading).toBe(true)
+        expect(result.current.data).toBe(undefined)
+        expect(result.current.error).toBe(undefined)
+    })
+
+    it('should return value after successful async call', async () => {
+        const { result } = renderHook(() => useAsync(mockSuccess))
+
+        await waitFor(() => {
+            expect(result.current.isLoading).toBe(false)
+            expect(result.current.data).toEqual({ data: 'Mock data' })
+            expect(result.current.error).toBe(undefined)
+        })
+    })
+
+    it('should return error object after failed async call', async () => {
+        const { result } = renderHook(() => useAsync(mockError))
+
+        await waitFor(() => {
+            expect(result.current.isLoading).toBe(false)
+            expect(result.current.data).toBe(undefined)
+            expect(result.current.error).toEqual(new Error('An error occurred'))
+        })
+    })
+
+    it('should execute async function when execute method is called', async () => {
+        const { result } = renderHook(() => useAsync(mockSuccess, false))
+
+        expect(result.current.isLoading).toBe(false)
+
+        act(() => {
+            result.current.execute()
+        })
+
+        expect(result.current.isLoading).toBe(true)
+
+        await waitFor(() => {
+            expect(result.current.isLoading).toBe(false)
+            expect(result.current.data).toEqual({ data: 'Mock data' })
+            expect(result.current.error).toBe(undefined)
+        })
+    })
 })
--- src/utility/hooks/useAsync.ts
+++ src/utility/hooks/useAsync.ts
@@ -1,27 +1,75 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useReducer, useCallback } from 'react'
+
+type AsyncAction<T> =
+    | { type: 'loading' }
+    | { type: 'success'; payload: T }
+    | { type: 'error'; payload: Error }
+
+type AsyncState<T> = {
+    isLoading: boolean
+    isError: boolean
+    isSuccess: boolean
+    data: T | undefined
+    error: Error | undefined
+}
+
+const asyncReducer = <T>(
+    state: AsyncState<T>,
+    action: AsyncAction<T>
+): AsyncState<T> => {
+    switch (action.type) {
+        case 'loading':
+            return {
+                isLoading: true,
+                isError: false,
+                isSuccess: false,
+                data: undefined,
+                error: undefined,
+            }
+        case 'success':
+            return {
+                isSuccess: true,
+                isLoading: false,
+                isError: false,
+                data: action.payload,
+                error: undefined,
+            }
+        case 'error':
+            return {
+                isError: true,
+                isSuccess: false,
+                isLoading: false,
+                data: undefined,
+                error: action.payload,
+            }
+    }
+}
 
 const useAsync = <T>(asyncFunction: () => Promise<T>, immediate = true) => {
-    const [loading, setLoading] = useState(false)
-    const [value, setValue] = useState<T | null>(null)
-    const [error, setError] = useState<Error | null>(null)
+    const [state, dispatch] = useReducer<
+        React.Reducer<AsyncState<T>, AsyncAction<T>>
+    >(asyncReducer, {
+        isLoading: false,
+        isError: false,
+        isSuccess: false,
+        data: undefined,
+        error: undefined,
+    })
 
     // The execute function wraps asyncFunction and
-    // handles setting state for pending, value, and error.
+    // dispatches state updates for pending, value, and error.
     // useCallback ensures the below useEffect is not called
     // on every render, but only if asyncFunction changes.
     const execute = useCallback(() => {
-        setLoading(true)
-        setValue(null)
-        setError(null)
+        dispatch({ type: 'loading' })
 
-        return asyncFunction()
+        asyncFunction()
             .then((response) => {
-                setValue(response)
+                dispatch({ type: 'success', payload: response })
             })
             .catch((error) => {
-                setError(error)
+                dispatch({ type: 'error', payload: error })
             })
-            .finally(() => setLoading(false))
     }, [asyncFunction])
 
     // Call execute if we want to fire it right away.
@@ -33,7 +81,7 @@ const useAsync = <T>(asyncFunction: () => Promise<T>, immediate = true) => {
         }
     }, [execute, immediate])
 
-    return { execute, loading, value, error }
+    return { execute, ...state }
 }
 
 export default useAsync
