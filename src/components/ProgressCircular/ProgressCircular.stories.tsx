1e3cd52a27265958e3b1700937b370fb5fb6159f
frauschert <fabian.rauschert@freenet.de>
2023-04-21 22:35:16 +0200

    updated ProgressCircular story

--- src/components/ProgressCircular/ProgressCircular.stories.tsx
+++ src/components/ProgressCircular/ProgressCircular.stories.tsx
@@ -1,16 +1,24 @@
-import React, { ComponentProps } from 'react'
-import { Meta } from '@storybook/react/types-6-0'
-import { Story } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import ProgressCircular from './ProgressCircular'
 
-export default {
+const meta: Meta<typeof ProgressCircular> = {
     title: 'Components/ProgressCircular',
     component: ProgressCircular,
-} as Meta
-
-// Create a master template for mapping args to render the Input component
-const Template: Story<ComponentProps<typeof ProgressCircular>> = (args) => {
-    return <ProgressCircular {...args} />
 }
 
-export const Default = Template.bind({})
+export default meta
+
+type Story = StoryObj<typeof ProgressCircular>
+
+export const Template: Story = {
+    args: {
+        progress: 20,
+    },
+}
+
+export const SpinnerMode: Story = {
+    args: {
+        ...Template.args,
+        spinnerMode: true,
+    },
+}
