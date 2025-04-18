69fa4d4aad3be33432b4621895d625848216a12c
Fabian Rauschert <fabian.rauschert@freenet.de>
2025-04-18 21:16:24 +0200

    feat: add stories for SkeletonImage and Ellipsis components with default configurations

--- src/components/Skeleton/SkeletonImage.stories.tsx
+++ src/components/Skeleton/SkeletonImage.stories.tsx
@@ -1,5 +1,6 @@
 import type { StoryObj, Meta } from '@storybook/react'
 import { SkeletonImage } from './Skeleton'
+import React from 'react'
 
 const meta: Meta<typeof SkeletonImage> = {
     title: 'Components/Skeleton/SkeletonImage',
@@ -10,4 +11,13 @@ export default meta
 
 type Story = StoryObj<typeof SkeletonImage>
 
-export const Template: Story = {}
+export const Template: Story = {
+    render: () => <SkeletonImage />,
+    args: {
+        width: 200,
+        height: 200,
+        borderRadius: 8,
+        animate: true,
+    },
+    name: 'Default',
+}
--- src/components/Spinner/Ellipsis/Ellipsis.stories.tsx
+++ src/components/Spinner/Ellipsis/Ellipsis.stories.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import type { StoryObj, Meta } from '@storybook/react'
 import Ellipsis from './Ellipsis'
+import hex from '../../../utility/hex'
 
 const meta: Meta<typeof Ellipsis> = {
     title: 'Components/Spinner/Ellipsis',
@@ -14,4 +16,11 @@ export default meta
 
 type Story = StoryObj<typeof Ellipsis>
 
-export const Template: Story = {}
+export const Template: Story = {
+    render: (args) => <Ellipsis {...args} />,
+    args: {
+        color: hex('#000'),
+        size: 32,
+    },
+    name: 'Default',
+}
