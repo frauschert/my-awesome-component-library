ce6812d5d8c212009923ce063638835685078e28
Fabian Rauschert <fabian.rauschert@freenet.de>
2025-04-21 22:57:53 +0200

    docs: update JSDoc comments for useMemoCompare hook to improve clarity and detail

--- src/utility/hooks/useMemoCompare.ts
+++ src/utility/hooks/useMemoCompare.ts
@@ -1,5 +1,14 @@
 import { useEffect, useRef } from 'react'
 
+/**
+ * useMemoCompare is a custom hook that compares the current value with the previous value
+ * using a custom comparison function. If the values are equal, it returns the previous value;
+ * otherwise, it returns the new value.
+ *
+ * @param next - The new value to compare.
+ * @param compare - A function that compares the previous and next values.
+ * @returns The previous value if equal; otherwise, the new value.
+ */
 export default function useMemoCompare<T>(
     next: T,
     compare: (prev: T, next: T) => boolean
