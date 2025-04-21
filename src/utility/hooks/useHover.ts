1398040cc2d3a746a186e8f9371d6317276e3944
Fabian Rauschert <fabian.rauschert@freenet.de>
2025-04-21 23:03:44 +0200

    docs: enhance JSDoc comments for useHover hook to improve clarity and detail

--- src/utility/hooks/useHover.ts
+++ src/utility/hooks/useHover.ts
@@ -2,6 +2,12 @@ import { RefObject } from 'react'
 import useEventListener from './useEventListener'
 import useToggle from './useToggle'
 
+/**
+ * Custom hook to track hover state of an element.
+ *
+ * @param {RefObject<HTMLElement>} ref - The ref of the element to track.
+ * @returns {boolean} - True if the element is hovered, false otherwise.
+ */
 export default function useHover(ref: RefObject<HTMLElement>) {
     const [hovered, toggle] = useToggle(false)
 
