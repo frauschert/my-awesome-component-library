69f0e0c1ed1692bdfc20508831cead53208be259
Fabian Rauschert <fabian.rauschert@freenet.de>
2025-04-21 23:00:32 +0200

    docs: update JSDoc comments for useLatestRef hook to enhance clarity and detail

--- src/utility/hooks/useLatestRef.ts
+++ src/utility/hooks/useLatestRef.ts
@@ -1,5 +1,12 @@
 import { useRef, useLayoutEffect } from 'react'
 
+/**
+ * A custom hook that returns a mutable ref object that holds the latest value.
+ * This is useful for keeping track of the latest value without causing re-renders.
+ *
+ * @param value - The value to be stored in the ref.
+ * @returns A ref object containing the latest value.
+ */
 export function useLatestRef<T>(value: T) {
     const ref = useRef<T>(value)
     useLayoutEffect(() => {
