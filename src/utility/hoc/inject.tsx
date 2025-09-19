e31e35595c92a85346953a1e0e932012cc0a255f
Fabian Rauschert <fabian.rauschert@freenet.de>
2025-09-19 22:09:41 +0200

    fix: remove unnecessary JSX.Element return type annotation in withInjectedProps

--- src/utility/hoc/inject.tsx
+++ src/utility/hoc/inject.tsx
@@ -13,7 +13,7 @@ export function withInjectedProps<U extends Record<string, unknown>>(
     injectedProps: U
 ) {
     return function <T extends U>(Component: React.ComponentType<T>) {
-        return function Injected(props: Omit<T, keyof U>): JSX.Element {
+        return function Injected(props: Omit<T, keyof U>) {
             //A type coercion is neccessary because TypeScript doesn't know that the Omit<T, "owner"> + {owner: ...} = T
             const newProps = { ...props, ...injectedProps } as T
             return <Component {...newProps} />
